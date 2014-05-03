a51d646cf3da3fff13fc8e5666dabad4ada9fea3
Jared Bunting <jared.bunting@peachjean.com>
2014-05-03 18:20:38 -0500

    Trying to get the basedir set properly for heroku's static files serving.

--- pinch/pinch/settings/production.py
+++ pinch/pinch/settings/production.py
@@ -83,7 +83,7 @@ ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
