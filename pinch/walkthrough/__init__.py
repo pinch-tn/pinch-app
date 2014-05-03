5ba519c53c87691ac555462f3b9f963979f22ba9
Cale Mooth <cale@midnightcheese.com>
2014-05-03 11:13:59 -0500

    Tempalte/views

--- pinch/walkthrough/__init__.py
+++ pinch/walkthrough/__init__.py
@@ -5,4 +5,8 @@ import views
 urls = patterns('',
                 url("^create_project/", views.create_project),
                 url("^big_idea/", views.big_idea),
+                url("^create_mvp/", views.create_mvp),
+                url("^gravity_board/", views.gravity_board),
+                url("^minify_mvp/", views.minify_mvp),
+                url("^validate/", views.validate),
                 )
--- pinch/walkthrough/views.py
+++ pinch/walkthrough/views.py
@@ -10,4 +10,15 @@ def create_project(request):
 def big_idea(request):
     return render(request, "big_idea.html")
 
+def create_mvp(request):
+    return render(request, "create_mvp.html")
 
+def gravity_board(request):
+    return render(request, "gravity_board.html")
+
+def minify_mvp(request):
+    return render(request, "minify_mvp.html")
+
+def validate(request):
+    return render(request, "validate.html")
+    
