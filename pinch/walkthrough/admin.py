e08b1d54242e4a65cbb388e28f826932d2f1b50b
Jared Bunting <jared.bunting@peachjean.com>
2014-05-03 11:12:55 -0500

    Project model done.

--- pinch/walkthrough/admin.py
+++ pinch/walkthrough/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from models import Project
 
-# Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('project_name', 'created', 'started', 'ended')
+
+admin.site.register(Project, ProjectAdmin)
--- pinch/walkthrough/models.py
+++ pinch/walkthrough/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Project(models.Model):
     project_name = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
-    started = models.DateTimeField()
-    ended = models.DateTimeField()
+    started = models.DateTimeField(blank=True, null=True, editable=False)
+    ended = models.DateTimeField(blank=True, null=True, editable=False)
 
-    idea = models.TextField()
-    validate_customer = models.TextField()
-    validate_offering = models.TextField()
-    validate_value_prop = models.TextField()
+    idea = models.TextField(blank=True)
+    validate_customer = models.TextField(blank=True)
+    validate_offering = models.TextField(blank=True)
+    validate_value_prop = models.TextField(blank=True)
