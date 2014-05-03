3bde627ed6db50dffd55658fb8a072027c06c918
Jared Bunting <jared.bunting@peachjean.com>
2014-05-03 13:41:55 -0500

    Moving project name url to the end so that we don't interfere with other urls.

--- pinch/walkthrough/__init__.py
+++ pinch/walkthrough/__init__.py
@@ -5,11 +5,11 @@ import views
 
 urls = patterns('',
                 # url("^$", redirect("create_project")),
-                url("^(?P<name>[A-Za-z0-9_-]+)/$", views.RootProjectView.as_view(), name="root_project"),
                 url("^create_project/$", views.CreateProjectView.as_view(), name="create_project"),
                 url("^big_idea/(?P<name>[A-Za-z0-9_-]+)/$", views.BigIdeaView.as_view(), name="big_idea"),
                 url("^create_mvp/(?P<name>[A-Za-z0-9_-]+)/$", views.CreateMvpView.as_view(), name="create_mvp"),
                 url("^gravity_board/(?P<name>[A-Za-z0-9_-]+)/$", views.GravityBoardView.as_view(), name="gravity_board"),
                 url("^minify_mvp/(?P<name>[A-Za-z0-9_-]+)/$", views.MinifyMvpView.as_view(), name="minify_mvp"),
                 url("^validate/(?P<name>[A-Za-z0-9_-]+)/$", views.ValidateView.as_view(), name="validate"),
+                url("^(?P<name>[A-Za-z0-9_-]+)/$", views.RootProjectView.as_view(), name="root_project"),
                 )
