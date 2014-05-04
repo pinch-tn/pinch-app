823dc6a6dbf2dd4fb7ab716def6eb18b55cd674a
Matt Conrad <mmconrad@gmail.com>
2014-05-04 12:20:51 -0500

    drag testing

--- pinch/static/js/pinch.js
+++ pinch/static/js/pinch.js
@@ -82,41 +82,43 @@ $(document).ready(function() {
 
 
 	// Drag and Drop sticky notes
-	var adjustment
-	var item			// Needed???  For each sticky note
+	// var adjustment
+	// var item			// Needed???  For each sticky note
 
-	$(".task-columns").sortable({
-	  group: 'column',
-	  pullPlaceholder: false,
-	  // animation on drop
-	  onDrop: function  (item, targetContainer, _super) {
-	    var clonedItem = $('<li/>').css({height: 0})
-	    item.before(clonedItem)
-	    clonedItem.animate({'height': item.height()})
+	// $(".task-columns").sortable({
+	//   group: 'column',
+	//   pullPlaceholder: false,
+	//   // animation on drop
+	//   onDrop: function  (item, targetContainer, _super) {
+	//     var clonedItem = $('<li/>').css({height: 0})
+	//     item.before(clonedItem)
+	//     clonedItem.animate({'height': item.height()})
 	    
-	    item.animate(clonedItem.position(), function  () {
-	      clonedItem.detach()
-	      _super(item)
-	    })
-	  },
+	//     item.animate(clonedItem.position(), function  () {
+	//       clonedItem.detach()
+	//       _super(item)
+	//     })
+	//   },
 
-	  // set item relative to cursor position
-	  onDragStart: function ($item, container, _super) {
-	    var offset = $item.offset(),
-	    pointer = container.rootGroup.pointer
+	//   // set item relative to cursor position
+	//   onDragStart: function ($item, container, _super) {
+	//     var offset = $item.offset(),
+	//     pointer = container.rootGroup.pointer
 
-	    adjustment = {
-	      left: pointer.left - offset.left,
-	      top: pointer.top - offset.top
-	    }
+	//     adjustment = {
+	//       left: pointer.left - offset.left,
+	//       top: pointer.top - offset.top
+	//     }
 
-	    _super($item, container)
-	  },
-	  onDrag: function ($item, position) {
-	    $item.css({
-	      left: position.left - adjustment.left,
-	      top: position.top - adjustment.top
-	    })
-	  }
-	});
+	//     _super($item, container)
+	//   },
+	//   onDrag: function ($item, position) {
+	//     $item.css({
+	//       left: position.left - adjustment.left,
+	//       top: position.top - adjustment.top
+	//     })
+	//   }
+	// });
+
+
 });
