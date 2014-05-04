5bef4e6232255f15b8cde712150156cc749c698d
Jared Bunting <jared.bunting@peachjean.com>
2014-05-03 21:19:43 -0500

    Trying to get the basedir set properly for heroku's static files serving (try 18).

--- pinch/pinch/settings/production.py
+++ pinch/pinch/settings/production.py
@@ -24,7 +24,7 @@ def get_env_setting(setting):
 
 ########## HOST CONFIGURATION
 # See: https://docs.djangoproject.com/en/1.5/releases/1.5/#allowed-hosts-required-in-production
-ALLOWED_HOSTS = ['http://pinch-tn.herokuapp.com']
+ALLOWED_HOSTS = ['pinch-tn.herokuapp.com']
 ########## END HOST CONFIGURATION
 
 ########## EMAIL CONFIGURATION
