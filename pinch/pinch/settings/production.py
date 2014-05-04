106f69f727273be895509003dd75d59bb1e83f4e
Jared Bunting <jared.bunting@peachjean.com>
2014-05-03 20:54:38 -0500

    Trying to get the basedir set properly for heroku's static files serving (try 13).

--- pinch/pinch/settings/production.py
+++ pinch/pinch/settings/production.py
@@ -19,6 +19,9 @@ def get_env_setting(setting):
         error_msg = "Set the %s env variable" % setting
         raise ImproperlyConfigured(error_msg)
 
+
+DEBUG=True
+
 ########## HOST CONFIGURATION
 # See: https://docs.djangoproject.com/en/1.5/releases/1.5/#allowed-hosts-required-in-production
 ALLOWED_HOSTS = []
