e9df831ea602f67f7d3e2a10559e8701004fc06c
Jared Bunting <jared.bunting@peachjean.com>
2014-05-05 10:20:04 -0500

    Improved knockout model and columns have unique classes based on workstream.

--- pinch/static/js/gravity_board.js
+++ pinch/static/js/gravity_board.js
@@ -1,174 +1,196 @@
-function GravityBoardModel() {
-	this.workstreams = ko.observableArray([
-		                                      {
-			                                      name: ko.observable("first stream"),
-			                                      ready: {
-				                                      tickets: ko.observableArray([
-					                                                                  {
-						                                                                  id: 1,
-						                                                                  text: "ticket 1"
-					                                                                  },
-					                                                                  {
-						                                                                  id: 2,
-						                                                                  text: "ticket 2"
-					                                                                  }
-
-				                                                                  ])
-			                                      },
-			                                      doing: {
-				                                      tickets: ko.observableArray([
-					                                                                  {
-						                                                                  id: 3,
-						                                                                  text: "ticket 3"
-					                                                                  },
-					                                                                  {
-						                                                                  id: 4,
-						                                                                  text: "ticket 4"
-					                                                                  }
-
-				                                                                  ])
-			                                      },
-			                                      done: {
-				                                      tickets: ko.observableArray([
-					                                                                  {
-						                                                                  id: 5,
-						                                                                  text: "ticket 5"
-					                                                                  },
-					                                                                  {
-						                                                                  id: 6,
-						                                                                  text: "ticket 6"
-					                                                                  }
-
-				                                                                  ])
-			                                      }
-		                                      },
-		                                      {
-			                                      name: ko.observable("second stream"),
-			                                      ready: {
-				                                      tickets: ko.observableArray([
-					                                                                  {
-						                                                                  id: 11,
-						                                                                  text: "ticket 11"
-					                                                                  },
-					                                                                  {
-						                                                                  id: 12,
-						                                                                  text: "ticket 12"
-					                                                                  }
-
-				                                                                  ])
-			                                      },
-			                                      doing: {
-				                                      tickets: ko.observableArray([
-					                                                                  {
-						                                                                  id: 13,
-						                                                                  text: "ticket 13"
-					                                                                  },
-					                                                                  {
-						                                                                  id: 14,
-						                                                                  text: "ticket 14"
-					                                                                  }
-
-				                                                                  ])
-			                                      },
-			                                      done: {
-				                                      tickets: ko.observableArray([
-					                                                                  {
-						                                                                  id: 15,
-						                                                                  text: "ticket 15"
-					                                                                  },
-					                                                                  {
-						                                                                  id: 16,
-						                                                                  text: "ticket 16"
-					                                                                  }
-
-				                                                                  ])
-			                                      }
-		                                      },
-		                                      {
-			                                      name: ko.observable("third stream"),
-			                                      ready: {
-				                                      tickets: ko.observableArray([
-					                                                                  {
-						                                                                  id: 21,
-						                                                                  text: "ticket 21"
-					                                                                  },
-					                                                                  {
-						                                                                  id: 22,
-						                                                                  text: "ticket 22"
-					                                                                  }
-
-				                                                                  ])
-			                                      },
-			                                      doing: {
-				                                      tickets: ko.observableArray([
-					                                                                  {
-						                                                                  id: 23,
-						                                                                  text: "ticket 23"
-					                                                                  },
-					                                                                  {
-						                                                                  id: 24,
-						                                                                  text: "ticket 24"
-					                                                                  }
-
-				                                                                  ])
-			                                      },
-			                                      done: {
-				                                      tickets: ko.observableArray([
-					                                                                  {
-						                                                                  id: 25,
-						                                                                  text: "ticket 25"
-					                                                                  },
-					                                                                  {
-						                                                                  id: 26,
-						                                                                  text: "ticket 26"
-					                                                                  }
-
-				                                                                  ])
-			                                      }
-		                                      },
-		                                      {
-			                                      name: ko.observable("Tools and Technology"),
-			                                      ready: {
-				                                      tickets: ko.observableArray([
-					                                                                  {
-						                                                                  id: 31,
-						                                                                  text: "ticket 31"
-					                                                                  },
-					                                                                  {
-						                                                                  id: 32,
-						                                                                  text: "ticket 32"
-					                                                                  }
-
-				                                                                  ])
-			                                      },
-			                                      doing: {
-				                                      tickets: ko.observableArray([
-					                                                                  {
-						                                                                  id: 33,
-						                                                                  text: "ticket 33"
-					                                                                  },
-					                                                                  {
-						                                                                  id: 34,
-						                                                                  text: "ticket 34"
-					                                                                  }
-
-				                                                                  ])
-			                                      },
-			                                      done: {
-				                                      tickets: ko.observableArray([
-					                                                                  {
-						                                                                  id: 35,
-						                                                                  text: "ticket 35"
-					                                                                  },
-					                                                                  {
-						                                                                  id: 36,
-						                                                                  text: "ticket 36"
-					                                                                  }
-
-				                                                                  ])
-			                                      }
-		                                      }
-	                                      ])
+function convertToSlug(Text)
+{
+	return Text
+		.toLowerCase()
+		.replace(/[^\w ]+/g, '')
+		.replace(/ +/g, '-')
+		;
 }
 
-ko.applyBindings(new GravityBoardModel());
+function TicketModel(ticket)
+{
+	this.id = ko.observable(ticket.id);
+	this.text = ko.observable(ticket.text)
+}
+
+function WorkstreamStatusModel(status, tickets)
+{
+	this.status = status;
+	this.tickets = ko.observableArray([]);
+
+	for (var i = 0; i < tickets.length; i++)
+	{
+		this.tickets.push(new TicketModel(tickets[i]))
+	}
+}
+
+function WorkstreamModel(name, readyTickets, doingTickets, doneTickets)
+{
+	this.name = ko.observable(name);
+	this.cssClass = ko.computed(function ()
+	                            {
+		                            return convertToSlug(this.name());
+	                            }, this);
+	this.ready = new WorkstreamStatusModel("ready", readyTickets);
+	this.doing = new WorkstreamStatusModel("doing", doingTickets);
+	this.done = new WorkstreamStatusModel("done", doneTickets);
+}
+
+function GravityBoardModel(workstreams)
+{
+	this.workstreams = ko.observableArray([]);
+	for (var i = 0; i < workstreams.length; i++)
+	{
+		var ws = workstreams[i];
+		this.workstreams.push(new WorkstreamModel(ws.name, ws.ready, ws.doing, ws.done));
+	}
+}
+
+var mockTicketSetup = [
+	{
+		name: "first stream",
+		ready: [
+			{
+				id: 1,
+				text: "ticket 1"
+			},
+			{
+				id: 2,
+				text: "ticket 2"
+			}
+
+		],
+		doing: [
+			{
+				id: 3,
+				text: "ticket 3"
+			},
+			{
+				id: 4,
+				text: "ticket 4"
+			}
+
+		],
+		done: [
+			{
+				id: 5,
+				text: "ticket 5"
+			},
+			{
+				id: 6,
+				text: "ticket 6"
+			}
+
+		]
+	},
+	{
+		name: "second stream",
+		ready: [
+			{
+				id: 11,
+				text: "ticket 11"
+			},
+			{
+				id: 12,
+				text: "ticket 12"
+			}
+
+		],
+		doing: [
+			{
+				id: 13,
+				text: "ticket 13"
+			},
+			{
+				id: 14,
+				text: "ticket 14"
+			}
+
+		],
+		done: [
+			{
+				id: 15,
+				text: "ticket 15"
+			},
+			{
+				id: 16,
+				text: "ticket 16"
+			}
+
+		]
+	},
+	{
+		name: "third stream",
+		ready: [
+			{
+				id: 21,
+				text: "ticket 21"
+			},
+			{
+				id: 22,
+				text: "ticket 22"
+			}
+
+		],
+		doing: [
+			{
+				id: 23,
+				text: "ticket 23"
+			},
+			{
+				id: 24,
+				text: "ticket 24"
+			}
+
+		],
+		done: [
+			{
+				id: 25,
+				text: "ticket 25"
+			},
+			{
+				id: 26,
+				text: "ticket 26"
+			}
+
+		]
+	},
+	{
+		name: "Tools and Technology",
+		ready: [
+			{
+				id: 31,
+				text: "ticket 31"
+			},
+			{
+				id: 32,
+				text: "ticket 32"
+			}
+
+		],
+		doing: [
+			{
+				id: 33,
+				text: "ticket 33"
+			},
+			{
+				id: 34,
+				text: "ticket 34"
+			}
+
+		],
+		done: [
+			{
+				id: 35,
+				text: "ticket 35"
+			},
+			{
+				id: 36,
+				text: "ticket 36"
+			}
+
+		]
+	}
+];
+
+ko.applyBindings(new GravityBoardModel(mockTicketSetup));
